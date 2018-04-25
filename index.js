f4259bf1a7c0553d5a65577afece620679563db5
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-25 13:56:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function getFirstSelector(selector){
+  let el = document.querySelector(selector);
+  return el;
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  
+}
