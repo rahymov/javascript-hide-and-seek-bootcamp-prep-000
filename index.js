3cd02d15f4bf8e1b0408d138f27191f73176b5b4
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-25 14:01:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget(){
 
 function increaseRankBy(n){
   let rnklists =  document.querySelectorAll('ranked-list') ;
-  for(var i=0; i < rnklists.length)
+  for(var i=0; i < rnklists.length; i++)
 }
 
 function deepestChild(){
