84dc3959769bddb5a8a50507b003b48dcebf62c5
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-25 14:11:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function increaseRankBy(n){
 function deepestChild(){
   let dchild = document.getElementById('grand-node').querySelectorAll('div');
   for(var i=0; i < dchild.length-1 ; i++){
-    let result = dchild.querySelector('dic') ;
+    let result = dchild.querySelector('dicv') ;
   }
 }
