a23cd515e953607cd7001ac44c4f69022ee4655c
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-25 14:12:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector){
+  let el = document.querySelector(selector);
+  return el;
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  let rnklists =  document.querySelectorAll('.ranked-list') ;
+  for(var i=0; i < rnklists.length; i++){
+    rnklists[i].innerHTML = parseInt(rnklists[i].innerHTML)+n ;
+  }
+}
+
+function deepestChild(){
+  let dchild = document.getElementById('grand-node').querySelectorAll('div');
+  let result;
+  for(var i=0; i < dchild.length-1 ; i++){
+    result = dchild[i].querySelector('div') ;
+  }
+  return result;
+}
